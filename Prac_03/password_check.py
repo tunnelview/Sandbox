1697599a9f4314d4a26309d57fa1d928ada1c2cc
tunnelview <abrahamshibin1@gmail.com>
2021-08-20 20:07:24 +1000

    Add password check program

---
+++ Prac_03/password_check.py
@@ -0,0 +1,21 @@
+
+
+
+
+MINIMUM_LENGTH = 16
+
+get_password = input("Enter Password of at least {} characters: ".format(MINIMUM_LENGTH))
+while len(get_password) < MINIMUM_LENGTH:
+    print(f"please enter password of {MINIMUM_LENGTH} characters")
+    get_password = input("Enter Password of at least {} characters: ".format(MINIMUM_LENGTH))
+print("*" * len(get_password))
+# if len(get_password) < 10:
+#
+# elif len(get_password) > 10 :
+#     print("Your password is too long, try again!")
+# else:
+#     print("Your password length is correct")
+# print("Your password is masked")
+
+
+
